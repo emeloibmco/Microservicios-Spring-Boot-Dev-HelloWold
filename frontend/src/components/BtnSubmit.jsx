dd881988604a16c31252f2a9525c8feaee409657
sofiaponteb <aaponte@unal.edu.co>
2023-03-08 17:40:43 -0500

    CORS error

--- frontend/src/components/BtnSubmit.jsx
+++ frontend/src/components/BtnSubmit.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react"
 import Button from "./shared/Button"
 import ChatContext from "../context/ChatContext"
+import { v4 } from 'uuid'
 
 
 function BtnSubmit() {
     const role = 'hola'
+    const id = v4()
 
     const {textHola, addChat, getHola} = useContext(ChatContext)
 
@@ -13,21 +15,35 @@ function BtnSubmit() {
         
         const options = {method: 'GET',mode:'no-cors'};
 
-        const saludo = fetch('http://localhost:65230/saludar', options)
-        .then(response => response.text())
+        const saludo = fetch('http://localhost:65230/saludar', {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+          "Accept": "application/json",
+          'Content-Type': 'application/json'
+          }
+        }
+
+        )
+        .then(response => response.json())
         .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+        .catch(err => {
+          console.log("fetch error " + err)
+          //console.error(err)
+        });
 
         let text 
         const printSaludo = async() => {
         text = await saludo;
         console.log(text);
         }
-        console.log(text)
+        //console.log(text)
         //const text = textHola 
         const newChat = {
             text,
             role,
+            id,
         }
         addChat(newChat)
         //console.log(newChat)
